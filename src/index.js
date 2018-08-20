d50d655ca345f0e40ee178af93ef3251a6ae645a
JestemBartek <maciek.bartynski@gmail.com>
2018-08-20 14:21:46 +0200

    works

--- src/index.js
+++ src/index.js
@@ -1,43 +1,10 @@
 import './index.scss';
-import axios from 'axios';
 import {
-    GetData
-} from './scripts/import.js';
+    SearchButton
+} from './scripts/SearchButton';
 'use strict';
-class SearchPanel {
-    constructor(string) {
-        this.string = string;
-    }
-    modifyAllThings(j){
-        for (let i=0; i<4; i++){
-            let datas = new GetData(i,j);
-            datas.dataSet(this.string);
-        }
-    }
-}
-class BtnSearcher {
-    constructor(){
-        this.j=0;
-    }
-    onClick(str, j){
-        let starter = new SearchPanel(str);
-        starter.modifyAllThings(j);
-    }
-    listenForIt(){
-        let its = this;
-        let button = document.querySelector('.sn001_container1_searchbutton');
-        button.addEventListener('click', function(){
-            let searchfield = document.querySelector('input').value;
-            its.onClick(searchfield, this.j);
-            this.j=this.j+1;
-            this.j = this.j < 10 ? this.j : 0;
-            console.log(this.j)
-            document.querySelector('.sn001_container2').classList.add('itsClicked');
-        })
-    }
-}
 document.addEventListener('DOMContentLoaded', function(){
-    let btn = new BtnSearcher;
-    btn.listenForIt();
+    let btn = new SearchButton;
+    btn.start();
 })
 
